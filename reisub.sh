4c58b5f16ad142bb752f3ebcae5d4f709641104b
Woody the Fox <111209874+NeonHaiiroookami@users.noreply.github.com>
2026-06-01 19:34:49 -0700

    Update reisub.sh

--- reisub.sh
+++ reisub.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 echo r | sudo tee /proc/sysrq-trigger
+sleep 2.5
 echo e | sudo tee /proc/sysrq-trigger
+sleep 2.5
 echo i | sudo tee /proc/sysrq-trigger
+sleep 2.5
 echo s | sudo tee /proc/sysrq-trigger
+sleep 2.5
 echo u | sudo tee /proc/sysrq-trigger
+sleep 2.5
 echo b | sudo tee /proc/sysrq-trigger

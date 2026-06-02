2888524912aa5799cec5ff9d75dbfe3c6b517bae
Woody the Fox <111209874+NeonHaiiroookami@users.noreply.github.com>
2026-06-01 19:28:31 -0700

    Create reisub.sh

---
+++ reisub.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo r | sudo tee /proc/sysrq-trigger
+echo e | sudo tee /proc/sysrq-trigger
+echo i | sudo tee /proc/sysrq-trigger
+echo s | sudo tee /proc/sysrq-trigger
+echo u | sudo tee /proc/sysrq-trigger
+echo b | sudo tee /proc/sysrq-trigger
